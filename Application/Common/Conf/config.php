9ebd559d334ea1161fc4fc6c766a98dcff9d5bf5
gxd <gxd@zy.com>
2019-11-08 16:13:33 +0800

    切换pgsql

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -31,12 +31,12 @@ return array(
     'URL_HTML_SUFFIX' => '', //URL伪静态后缀
 
     /* 数据库设置 */
-    'DB_TYPE' => 'mysql', // 数据库类型
+    'DB_TYPE' => 'pgsql', // 数据库类型
     'DB_HOST' => 'localhost', // 服务器地址
     'DB_NAME' => 'wfx', // 数据库名
-    'DB_USER' => 'root', // 用户名
-    'DB_PWD' => 'root', // 密码
-    'DB_PORT' => '3307', // 端口
+    'DB_USER' => 'postgres', // 用户名
+    'DB_PWD' => 'postgres', // 密码
+    'DB_PORT' => '5432', // 端口
     'DB_PREFIX' => 'wfx_', // 数据库表前缀
 
     'AUTOLOAD_NAMESPACE' => array(
